439d5b953b30161566968bf3357e306789d38832
ngc2207 <cfngc3109@gmail.com>
2024-10-30 16:30:26 +0800

    feat(@answer): add answer layout and code page component

---
+++ src/app/(main)/dashboard/playground/@answer/@code/page.tsx
@@ -0,0 +1,5 @@
+export default function CodePage() {
+  return (
+    <div>Code</div>
+  )
+}
--- src/app/(main)/dashboard/playground/@answer/layout.tsx
+++ src/app/(main)/dashboard/playground/@answer/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CodeXml } from "lucide-react";
 
 export default function AnswerLayout({
   children,
@@ -8,9 +9,14 @@ export default function AnswerLayout({
   return (
     <div className="h-full w-full border rounded-lg">
       {children}
-      <Tabs>
-        <TabsList>
-          <TabsTrigger value="code">代码编辑器</TabsTrigger>
+      <Tabs defaultValue="code">
+        <TabsList className="flex flex-row items-center justify-start rounded-t-lg rounded-b-none">
+          <TabsTrigger value="code" className="rounded-lg">
+            <div className="flex flex-row items-center justify-start">
+              <CodeXml className="h-6 w-auto text-cyan-500" />
+              <span className="ml-2">代码</span>
+            </div>
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="code">{code}</TabsContent>
       </Tabs>
