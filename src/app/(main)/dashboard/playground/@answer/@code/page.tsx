ec47281f4858a34e621d58200e2a979389861887
ngc2207 <cfngc3109@gmail.com>
2024-10-30 19:02:30 +0800

    feat(@code): add coding tools and tooltip for them

--- src/app/(main)/dashboard/playground/@answer/@code/page.tsx
+++ src/app/(main)/dashboard/playground/@answer/@code/page.tsx
@@ -7,23 +7,72 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import Editor from "@monaco-editor/react";
+import { Play, RefreshCw, Save, Settings } from "lucide-react";
 
 export default function CodePage() {
   return (
     <div className="h-full w-full flex flex-col">
-      <div className="h-8 w-full border-b p-1">
+      <div className="h-8 w-full border-b p-1 flex flex-row items-center justify-between">
         <div className="flex flex-row">
           <Select>
             <SelectTrigger className="h-6 w-auto border-none px-1.5 py-0.5">
               <SelectValue placeholder="C" defaultValue="c" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">C</SelectItem>
-              <SelectItem value="dark">C++</SelectItem>
-              <SelectItem value="system">Java</SelectItem>
+              <SelectItem value="c">C</SelectItem>
+              <SelectItem value="cpp">C++</SelectItem>
+              <SelectItem value="java">Java</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+        <div className="flex flex-row items-center gap-2 mr-4">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <RefreshCw className="h-4 w-auto cursor-pointer" />
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={9}>
+                <p>重置</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Settings className="h-4 w-auto cursor-pointer" />
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={9}>
+                <p>设置</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Play className="h-4 w-auto cursor-pointer" />
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={9}>
+                <p>运行</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Save className="h-4 w-auto cursor-pointer" />
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={9}>
+                <p>保存</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
       <div className="flex-grow">
