73ceff6301674f2e324918c7e72094b9195e1efd
ngc2207 <cfngc3109@gmail.com>
2024-10-30 18:18:38 +0800

    feat(@answer): add select card for code languages

--- src/app/(main)/dashboard/playground/@answer/@code/page.tsx
+++ src/app/(main)/dashboard/playground/@answer/@code/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import Editor from "@monaco-editor/react";
 
 export default function CodePage() {
   return (
     <div className="h-full w-full flex flex-col">
-      <div className="h-8 w-full border-b p-1">Tab</div>
+      <div className="h-8 w-full border-b p-1">
+        <div className="flex flex-row">
+          <Select>
+            <SelectTrigger className="h-6 w-auto border-none px-1.5 py-0.5">
+              <SelectValue placeholder="C" defaultValue="c" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">C</SelectItem>
+              <SelectItem value="dark">C++</SelectItem>
+              <SelectItem value="system">Java</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
       <div className="flex-grow">
         <Editor
           height="100%"
--- src/app/(main)/dashboard/playground/@answer/layout.tsx
+++ src/app/(main)/dashboard/playground/@answer/layout.tsx
@@ -7,19 +7,29 @@ export default function AnswerLayout({
   code,
 }: Readonly<{ children: React.ReactNode; code: React.ReactNode }>) {
   return (
-    <div className="h-full w-full border rounded-lg">
+    <div className="h-full w-full flex flex-col border rounded-lg">
       {children}
-      <Tabs defaultValue="code" className="h-full w-full flex flex-col">
-        <TabsList className="flex flex-row items-center justify-start rounded-b-none h-9">
-          <TabsTrigger value="code" className="rounded-full h-7 px-2 py-1">
-            <div className="flex flex-row items-center justify-start">
-              <CodeXml className="h-6 w-auto text-cyan-500" />
-              <span className="ml-2">代码</span>
-            </div>
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="code" className="flex-grow mt-0">{code}</TabsContent>
-      </Tabs>
+      <div className="flex-grow">
+        <Tabs defaultValue="code" className="h-full w-full flex flex-col">
+          <TabsList className="flex flex-row items-center justify-start rounded-b-none h-9">
+            <TabsTrigger value="code" className="rounded-full h-7 px-2 py-1">
+              <div className="flex flex-row items-center justify-start">
+                <CodeXml className="h-6 w-auto text-cyan-500" />
+                <span className="ml-2">代码</span>
+              </div>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="code" className="flex-grow mt-0">
+            {code}
+          </TabsContent>
+        </Tabs>
+      </div>
+      <div className="h-9 w-full flex items-center px-3 py-2 border-t">
+        <div className="flex flex-1 items-center justify-between">
+          <span className="text-sm">已存储</span>
+          <span className="text-sm">行30,列10</span>
+        </div>
+      </div>
     </div>
   );
 }
